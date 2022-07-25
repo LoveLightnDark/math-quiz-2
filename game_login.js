ef016f652ccc064590433b96a73fd9a5127e12f2
LoveLightnDark <100453891+LoveLightnDark@users.noreply.github.com>
2022-07-25 19:59:31 -0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,12 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+  localStorage.getItem("player1_name").value;
+  localStorage.getItem("player2_name").value;
+    //Set "player1_name" using localStorage.setItem() function
+	  //Set "player2_name" using localStorage.setItem() function
+
+    window.location = "game_page.html";
+}
+
